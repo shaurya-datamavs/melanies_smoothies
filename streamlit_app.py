a352a7b8ac187a02084c86454c50ad8c09eb86fd
shaurya-datamavs <shaurya.kumar@datamavericks.com>
2026-02-06 17:47:04 +0530

    Update streamlit_app.py
    Moved the display

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,9 @@ if ingridients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingridients_list:
-        ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' ' 
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -44,7 +46,3 @@ if ingridients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-      
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
